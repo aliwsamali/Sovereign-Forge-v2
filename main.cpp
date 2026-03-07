a188420c2082fd970b5049dee311889ea30a64fa
aliwsamali <clashik61@gmail.com>
2026-03-08 00:38:00 +0300

    Implement wsam_init function for GHOST RAT
    Added initialization function for GHOST RAT system.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * نظام GHOST RAT v2000 - النخاع السيادي
+ * المصمم: المهندس وسام الصافي - البصرة / الناصرية
+ * الحساب الجديد: aliwsamali
+ */
+void __attribute__((constructor)) wsam_init() {
+    printf("\n************************************************\n");
+    printf("[Sovereign] SUCCESS! DYLIB FORGED ON NEW ACCOUNT\n");
+    printf("[Sovereign] FOR GENERAL WSAM AL-SAFI\n");
+    printf("[Sovereign] Architecture: ARM64 (Pure Apple Core)\n");
+    printf("************************************************\n");
+}
